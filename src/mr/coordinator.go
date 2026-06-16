07e8827c73a59714a0ed04696b37928f7233ad7c
haoyu <jonhuo11@gmail.com>
2026-06-15 21:47:06 -0400

    coordinator wip

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,15 +1,32 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync/atomic"
+	"time"
+)
 
+const workerTimeout = 10 * time.Second
+
+type taskEvent struct {
+	attempt int // attempt == mapTask.attempt -> accept
+}
 
 type Coordinator struct {
 	// Your definitions here.
 
+	done       atomic.Bool
+	nReduce    int
+	inputFiles []string
+
+	mapAcks        chan taskEvent
+	mapTimeouts    chan taskEvent
+	reduceAcks     chan taskEvent
+	reduceTimeouts chan taskEvent
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -22,6 +39,21 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	return nil
 }
 
+/*
+func (c *Coordinator) ReqMap(args *ReqArgs, reply *ReqReply) error {
+
+	func timeout() {
+		time.Sleep(workerTimeout)
+
+	}
+	go timeout()
+}
+
+func (c *Coordinator) AckMap(...) error {
+	// map task is complete
+	defer c.mapAcks <- empty{}
+}
+*/
 
 // start a thread that listens for RPCs from worker.go
 func (c *Coordinator) server(sockname string) {
@@ -38,12 +70,7 @@ func (c *Coordinator) server(sockname string) {
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
 func (c *Coordinator) Done() bool {
-	ret := true
-
-	// Your code here.
-
-
-	return ret
+	return c.done.Load()
 }
 
 // create a Coordinator.
@@ -54,7 +81,47 @@ func MakeCoordinator(sockname string, files []string, nReduce int) *Coordinator 
 
 	// Your code here.
 
+	c.done.Store(false)
+	c.nReduce = nReduce
+	c.inputFiles = files
+
+	go c.mapReduce()
 
 	c.server(sockname)
 	return &c
+}
+
+// there will be 1 instance of this thread and it will be the only one modifying Coordinator
+// no locks needed, just one atomic on c.done
+func (c *Coordinator) mapReduce() {
+	type mapTask struct {
+		startedAt time.Time // timeout detection
+		attempt   int
+
+		file string
+	}
+
+	type reduceTask struct {
+		startedAt time.Time
+
+		tid uint // range from [0, nReduce)
+	}
+
+	defer c.done.Store(false)
+
+	// spawn one map task per input file
+	nMapTasks := len(c.inputFiles)
+	mapTasks := make([]mapTask, nMapTasks)
+	incompleteMapTasks := nMapTasks
+	for i, inputFile := range c.inputFiles {
+		mapTasks[i].done = false
+		mapTasks[i].file = inputFile
+	}
+	// wait for all map tasks to be claimed and completed with ACKs
+	for incompleteMapTasks > 0 {
+
+	}
+
+	// spawn nReduce number of reduce tasks
+	// wait for all reduce tasks to be claimed and completed with ACKs
 }
